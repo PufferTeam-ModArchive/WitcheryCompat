683b2ac76490ae19ba7ed4fbb394fdd381a2ef69
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2025-07-12 13:21:06 -0400

    Fix Witchery Stockade not connecting to modded stockade

--- src/main/java/net/pufferlab/witcherycompat/WitcheryCompat.java
+++ src/main/java/net/pufferlab/witcherycompat/WitcheryCompat.java
@@ -15,7 +15,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import net.pufferlab.witcherycompat.Tags;
 
-@Mod(modid = WitcheryCompat.MODID, version = Tags.VERSION, name = "WitcheryCompat", acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = WitcheryCompat.MODID, version = Tags.VERSION, name = "WitcheryCompat", acceptedMinecraftVersions = "[1.7.10]", dependencies = "required-after:gtnhmixins@[2.0.1,);" + "required-after:unimixins@[0.0.20,);")
 public class WitcheryCompat {
 
     public static final String MODID = "witcherycompat";
@@ -27,8 +27,6 @@ public class WitcheryCompat {
     public static Block stockade_bop;
 
     @Mod.EventHandler
-    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
-    // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit(event);
 
@@ -38,19 +36,16 @@ public class WitcheryCompat {
     }
 
     @Mod.EventHandler
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
         proxy.init(event);
     }
 
     @Mod.EventHandler
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
     }
 
     @Mod.EventHandler
-    // register server commands in this event handler (Remove if not needed)
     public void serverStarting(FMLServerStartingEvent event) {
         proxy.serverStarting(event);
     }
---
+++ src/main/java/net/pufferlab/witcherycompat/mixinplugin/WitcheryCompatEarlyMixins.java
@@ -0,0 +1,54 @@
+package net.pufferlab.witcherycompat.mixinplugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import net.minecraft.launchwrapper.Launch;
+
+@IFMLLoadingPlugin.Name("WitcheryCompatEarlyMixins")
+@IFMLLoadingPlugin.MCVersion("1.7.10")
+public class WitcheryCompatEarlyMixins implements IFMLLoadingPlugin, IEarlyMixinLoader  {
+	@Override
+	public String getMixinConfig() {
+		return "mixins.witcherycompat.early.json";
+	}
+
+	@Override
+	public List<String> getMixins(Set<String> loadedCoreMods) {
+
+		List<String> mixins = new ArrayList<>();
+
+		mixins.add("stockade.MixinBlockStockade");
+
+		return mixins;
+	}
+
+	@Override
+	public String[] getASMTransformerClass() {
+		return null;
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return null;
+	}
+
+	@Override
+	public String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+
+	}
+
+	@Override
+	public String getAccessTransformerClass() {
+		return null;
+	}
+}
